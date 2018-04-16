715fd61798a86b2dfe9a6b954a510f55873e7630
fang duan <df1228@gmail.com>
2018-04-16 13:02:24 +0800

    Add null to deal with nullable SQL and JSON values
    https://github.com/guregu/null
    see user.xo.go just column type sql.NullString to null.String
    
    All types implement sql.Scanner and driver.Valuer, so you can use this library in place of sql.NullXXX. All types also implement: encoding.TextMarshaler, encoding.TextUnmarshaler, json.Marshaler, and json.Unmarshaler.
    
    如果用string类型，碰到字段是null的时候，QueryRow.Scan 会报
    Scan error on column index 5: unsupported Scan, storing driver.Value type <nil> into type *string
    之类的错误
    
    默认xo生成的是sql.NullString
    json Marshal 或 Unmarshal 都需要特殊化处理

--- endpoints/session.go
+++ endpoints/session.go
@@ -1,7 +1,53 @@
 package endpoints
 
-import "net/http"
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
 
-func SessionEndpoint(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("not implemented"))
+	. "github.com/dfang/yuanxin/model"
+	. "github.com/dfang/yuanxin/util"
+)
+
+func SessionEndpoint(db *sql.DB) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// w.Write([]byte("not implemented"))
+
+		for k, v := range r.PostForm {
+			fmt.Printf("%s:%s\n", k, v)
+		}
+		phone := r.PostFormValue("phone")
+		password := r.PostFormValue("password")
+
+		if phone == "" {
+			RespondWithError(w, http.StatusServiceUnavailable, "缺少phone参数")
+		}
+
+		if phone == "" {
+			RespondWithError(w, http.StatusServiceUnavailable, "缺少password参数")
+		}
+
+		// user := User{
+		// 	Phone: r.PostFormValue("phone"),
+		// 	Pwd:   r.PostFormValue("password"),
+		// }
+
+		// use gorilla scheme to decode form values to user, that's called data binding in rails/asp.net mvc
+		// user := new(User)
+		// decoder := SchemaDecoder
+		// decoder :=
+		// schema.NewDecoder().Decode(&user, r.PostForm)
+		// decoder.Decode(user, r.PostForm)
+
+		// fmt.Printf("%+v\n", user)
+
+		// err := user.Sign
+		user, err := SignInUser(db, phone, password)
+
+		if err != nil {
+			RespondWithError(w, http.StatusServiceUnavailable, err.Error())
+		} else {
+			RespondWithJSON(w, http.StatusOK, user)
+		}
+	})
 }
