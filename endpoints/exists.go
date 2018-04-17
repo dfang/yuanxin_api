1ba9bfe5d6cc41f190dba3729e348f5b47ea9eac
fang duan <df1228@gmail.com>
2018-04-17 16:56:17 +0800

    FIX

--- endpoints/exists.go
+++ endpoints/exists.go
@@ -55,10 +55,6 @@ func captchaExists(w http.ResponseWriter, db *sql.DB, phone, code string) {
 
 func phoneExists(w http.ResponseWriter, db *sql.DB, phone string) {
 	user, err := model.UserByPhone(db, phone)
-<<<<<<< HEAD
-=======
-
->>>>>>> develop
 	if err != nil {
 		util.RespondWithJSON(w, http.StatusOK, struct {
 			StatusCode int    `json:"status_code"`
@@ -68,10 +64,6 @@ func phoneExists(w http.ResponseWriter, db *sql.DB, phone string) {
 			Message:    "手机号码没有被注册",
 		})
 	}
-<<<<<<< HEAD
-=======
-
->>>>>>> develop
 	if user != nil {
 		util.RespondWithJSON(w, http.StatusOK, struct {
 			StatusCode string `json:"status_code"`
