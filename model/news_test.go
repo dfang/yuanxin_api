f1ab900e7bf848b27296a03a5023b02fa257f663
fang duan <df1228@gmail.com>
2018-04-26 23:00:18 +0800

    Add test

--- model/news_test.go
+++ model/news_test.go
@@ -1,40 +1,30 @@
 package model
 
 import (
-	"database/sql"
-	"fmt"
-	"log"
-	"testing"
-
 	_ "github.com/go-sql-driver/mysql"
-	null "gopkg.in/guregu/null.v3"
 )
 
-func TestNewsItem_InsertNewsItem(t *testing.T) {
+// func TestNewsItem_InsertNewsItem(t *testing.T) {
+// 	db, mock, err := sqlmock.New()
+// 	if err != nil {
+// 		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+// 	}
+// 	defer db.Close()
 
-	item := NewsItem{
-		Title:       null.StringFrom("东芝存储芯片出售难产 债权银行施压其尽快完成交易"),
-		Link:        null.StringFrom("http://www.chinaflashmarket.com/Producer/TOSHIBA/News/160860"),
-		Type:        null.StringFrom("厂商动态"),
-		Description: null.StringFrom("据《金融时报》北京时间4月9日报道，银行业人士消息称，尽管有维权股东认为东芝存储芯片业务实际价值是出售价的两倍以上，但是东芝主要债权银行正督促东芝推进这笔2万亿日元(约合187亿美元)的出售交易。"),
-		Body:        null.StringFrom(""),
-		Source:      null.StringFrom("来源：凤凰科技"),
-	}
+// 	item := NewsItem{
+// 		Title:       null.StringFrom("东芝存储芯片出售难产 债权银行施压其尽快完成交易"),
+// 		Link:        null.StringFrom("http://www.chinaflashmarket.com/Producer/TOSHIBA/News/160860"),
+// 		Type:        null.StringFrom("厂商动态"),
+// 		Description: null.StringFrom("据《金融时报》北京时间4月9日报道，银行业人士消息称，尽管有维权股东认为东芝存储芯片业务实际价值是出售价的两倍以上，但是东芝主要债权银行正督促东芝推进这笔2万亿日元(约合187亿美元)的出售交易。"),
+// 		Body:        null.StringFrom(""),
+// 		Source:      null.StringFrom("来源：凤凰科技"),
+// 	}
 
-	// connectionString := fmt.Sprintf("%s:%s@%s/%s", "root", os.Getenv("APP_DB_PASSWORD"), os.Getenv("APP_DB_HOST"), os.Getenv("APP_DB_NAME"))
-	connectionString := fmt.Sprintf("%s:%s@%s/%s", "root", "", "tcp(localhost:3306)", "news")
-	log.Println(connectionString)
+// 	mock.ExpectExec("Insert news item").WillReturnResult(sqlmock.NewResult(1, 1))
 
-	db, err := sql.Open("mysql", connectionString)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
+// 	_, err = item.InsertNewsItem(db)
 
-	err = db.Ping()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	item.InsertNewsItem(db)
-}
+// 	if err := mock.ExpectationsWereMet(); err != nil {
+// 		t.Errorf("there were unfulfilled expectations: %s", err)
+// 	}
+// }
--- model/user_test.go
+++ model/user_test.go
@@ -1,7 +1,10 @@
 package model
 
 import (
+	"log"
 	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
 )
 
 func setupTestCase(t *testing.T) func(t *testing.T) {
@@ -18,5 +21,46 @@ func setupSubTest(t *testing.T) func(t *testing.T) {
 	}
 }
 func TestUser_InsertUser(t *testing.T) {
+
+}
+
+func TestUser_SignInUser(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+	// rows := sqlmock.NewRows([]string{"id", "title"}).
+	// 	AddRow(1, "one").
+	// 	AddRow(2, "two")
+
+	// mock.ExpectQuery("SELECT").WillReturnRows(rows)
+	// rs, _ := db.Query("SELECT")
+	// defer rs.Close()
+
+	// for rs.Next() {
+	// 	var id int
+	// 	var title string
+	// 	rs.Scan(&id, &title)
+	// 	fmt.Println("scanned id:", id, "and title:", title)
+	// }
+
+	// if rs.Err() != nil {
+	// 	fmt.Println("got rows error:", rs.Err())
+	// }
+
+	// rows := sqlmock.NewRows([]string{"phone", "pwd"}).AddRow("15618903080", "qaz123./")
+
+	mock.ExpectQuery("SELECT phone, pwd FROM news.users WHERE phone = ? AND pwd = ?").WithArgs("15618903080", "qaz123./").WillReturnRows()
+	// mock.ExpectQuery("SELECT (.+) FROM news.users WHERE phone = ? AND pwd = ?").WithArgs("15618903080", "qaz123./").WillReturnError(sqlmock.NewErrorResult())
+	// mock.ExpectQuery(sqlstr).WithArgs("15618903080", "qaz123./").WillReturnRows(sqlmock.NewRows([]string{"id", "nickname"}))
+	// ExpectExec("Sign In User").WithArgs(db, "15618903080", "qaz123./").WillReturnResult(sqlmock.NewResult(1, 1))
+
+	user, err := SignInUser(db, "15618903080", "qaz123./")
+
+	log.Println(user)
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
 
 }
