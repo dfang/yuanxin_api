ede430ea6d87a481c6e19c9d1f009f9ecddc4596
fang duan <df1228@gmail.com>
2018-04-29 08:03:46 +0800

    Implement favorable operations

--- endpoints/favorable.go
+++ endpoints/favorable.go
@@ -17,7 +17,36 @@ import (
 // 收藏和取消收藏
 func FavorableEndpoint(db *sql.DB) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("NOT IMPLEMENTED"))
+		// w.Write([]byte("NOT IMPLEMENTED"))
+
+		CheckRequiredParameters(r, "user_id", "favorable_type", "favorable_id")
+		var item model.Favorite
+		if err := util.SchemaDecoder.Decode(&item, r.PostForm); err != nil {
+			PanicIfNotNil(err)
+		}
+
+		favorite, err := model.GetFavoriteBy(db, item.FavorableType.String, item.FavorableID.Int64, item.UserID.Int64)
+		if favorite == nil || err != nil {
+			item.CreatedAt = null.TimeFrom(time.Now())
+			err := item.Insert(db)
+			PanicIfNotNil(err)
+			util.RespondWithJSON(w, http.StatusOK, struct {
+				StatusCode int            `json:"status_code"`
+				Message    string         `json:"msg"`
+				Favorite   model.Favorite `json:"favorite"`
+			}{
+				StatusCode: 200,
+				Message:    "收藏成功",
+				Favorite:   item,
+			})
+			return
+		} else {
+			err = favorite.Delete(db)
+			PanicIfNotNil(err)
+			util.RespondWithJSON(w, http.StatusOK, PayLoadFrom{200, "取消收藏成功"})
+			return
+		}
+
 	})
 }
 
