6dc211b186aafa95c0220f230b9b0e38c858ba72
fang duan <df1228@gmail.com>
2018-05-01 19:24:44 +0800

    Fix

--- endpoints/password.go
+++ endpoints/password.go
@@ -11,14 +11,23 @@ import (
 func PasswordEndpoint(db *sql.DB) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		CheckRequiredParameters(r, "phone", "code", "password")
-
 		r.ParseForm()
 
-		userID := GetUIDFromContext(r)
+		// userID := GetUIDFromContext(r)
+		_, err := model.CaptchaByPhoneAndCode(db, r.PostFormValue("phone"), r.PostFormValue("code"))
+		if err != nil {
+			util.RespondWithJSON(w, http.StatusOK, struct {
+				StatusCode int    `json:"status_code"`
+				Message    string `json:"msg"`
+			}{
+				StatusCode: 208,
+				Message:    "手机号或者验证码不对",
+			})
+			return
+		}
 
 		u2, _ := model.UserByPhone(db, r.PostFormValue("phone"))
-		if u2 != nil && u2.ID == userID {
-			// TODO: // 此处校验了手机号, 还需校验验证码
+		if u2 != nil {
 			// update password
 			u2.Pwd = hashAndSalt([]byte(r.PostFormValue("password")))
 			err := u2.UpdatePassword(db)
