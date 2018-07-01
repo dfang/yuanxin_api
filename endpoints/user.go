16d0d8af9b247bc19971f61707c6b0a33215d1cf
fang duan <df1228@gmail.com>
2018-07-01 19:38:55 +0800

    Refactoring

--- endpoints/user.go
+++ endpoints/user.go
@@ -5,10 +5,13 @@ import (
 	"errors"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
+	"time"
 
 	"github.com/dfang/yuanxin_api/model"
 	"github.com/dfang/yuanxin_api/util"
+	jwt "github.com/dgrijalva/jwt-go"
 	"golang.org/x/crypto/bcrypt"
 	null "gopkg.in/guregu/null.v3"
 
@@ -162,6 +165,58 @@ func PasswordEndpoint(db *sql.DB) http.HandlerFunc {
 			Message:    "密码修改失败",
 		})
 		return
+	})
+}
+
+func SessionEndpoint(db *sql.DB) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		CheckRequiredParameters(r, "password")
+		// TODO:  Validate phone format
+
+		phone := r.PostFormValue("phone")
+		email := r.PostFormValue("email")
+		password := r.PostFormValue("password")
+
+		// user, err := SignInUser(db, phone, hashedPassword)
+		user, err := model.UserByPhoneOrEmail(db, phone, email)
+		if err != nil || user == nil {
+			util.RespondWithJSON(w, http.StatusOK, PayLoadFrom{StatusCode: 205, Message: "手机号或密码错误"})
+			return
+		}
+
+		if user != nil {
+			err := bcrypt.CompareHashAndPassword([]byte(user.Pwd), []byte(password))
+			if err != nil {
+				util.RespondWithJSON(w, http.StatusOK, PayLoadFrom{StatusCode: 205, Message: "手机号或密码错误"})
+				return
+			}
+
+			// TODO: Touch login_date after successful login
+			token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+				"uid":        user.ID,
+				"last_login": time.Now(),
+			})
+
+			// Sign and get the complete encoded token as a string using the secret
+			tokenString, err := token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+			if err != nil {
+				panic("jwt sign token failed")
+			}
+
+			util.RespondWithJSON(w, http.StatusOK, struct {
+				StatusCode int         `json:"status_code"`
+				Message    string      `json:"msg"`
+				Data       *model.User `json:"data"`
+				JwtToken   string      `json:"token"`
+			}{
+				StatusCode: 200,
+				Message:    "查询成功",
+				Data:       user,
+				JwtToken:   tokenString,
+			})
+			return
+		}
 	})
 }
 
